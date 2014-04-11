aec21f8a44cbe6dc3b0eabebeacce47f6f1a3535
jennyoc <user@users-MacBook-Pro-3.local>
2014-04-11 11:11:52 -0400

    fsdfsdf

---
+++ before.php
@@ -0,0 +1,88 @@
+
+<!--$sitebuilder version="2.6" extra="Java(1.6.0_07)" md5="f464bfd757a397004884d8e07dddc900"$-->
+<!--$templateKey Animals & Pets|Paw Prints - Orange|2.0$-->
+<html>
+  <head>
+    <title>dog dogs cat cats store food essex jct vermont vt williston burlington stowe stowe,vt pet treats toys treat toy</title>
+    <meta name="generator" content="Yahoo! SiteBuilder/2.6/1.6.0_07">
+    <meta name="description" content="Natural dog and cat food and supplies, dog and cat toys and dog and cat treats, collars, leashes bowser dog beds">
+    <meta name="keywords" content="dog, cat, supplies, food, essex jct, vt, vermont, 05452, 05472, wellness, bravo, merrick ,nature's logic, primal,stella @ chewy's, iceland pure, natural balance,earthborn,fromm,taste of the wild,ziwi peak,orijen,zukes,before grain,weruva,instict,nature's variety, aunt jeni's, artemis, avoderm, bowswer dog beds, thedogandcat, essex junction, vermont pet store, pets stores, burlington, stowe, stowe,vt mountain, mountain rd.">
+    <!--$page size 1282, 3000$-->
+    <!--$page margin 1, 4, 1, 0$-->
+    <!--$fontFamily Arial$-->
+    <!--$fontSize 10$-->
+    <style type="text/css"><!--
+      BODY {font-family:"Arial"; font-size:10;margin:0px;padding:0px;}
+      P {font-family:"Arial"; font-size:10;}
+      FORM {margin:0;padding:0;}
+    --></style>
+    <!--$begin pageHtmlBefore$--> <META /="/" CONTENT="FoK9wO9u7QxBMqPvD+u1gKa7RjQ4UXOw4HqkRkC8P/0=" NAME="verify-v1">
+<META CONTENT="The Dog and Cat natural food and supplies pet store essex jct vt vermont essex jct., vt pet food vermont" NAME="Description" ID="Description"></META>
+<meta name="ABSTRACT" content="cats, dogs, toys, pet toys, cat toys, dog toys, fun toys, toys for cats, toys for pets, toys for dogs">
+<meta name="robots" content="index,follow">
+
+
+<!--$end pageHtmlBefore$-->
+  </head>
+  <body bgcolor="#FFFFFF" text="#FFFFFF" link="#0000FF" vlink="#800080" topmargin="0" leftmargin="0">
+
+<!--$begin exclude$-->
+    <div id="root" style="position:absolute;width:1281px;height:3000px;">
+<!--$end exclude$-->
+      <div id="e0" style="position:absolute;left:389;top:764;width:475;height:311;"><!--$img %ImageAssetImpl:/images/DSC_5659.jpg$--><img src="sitebuilder/images/DSC_5659-475x311.jpg" width="475" height="311" alt=""></div>      <div id="e1" style="position:absolute;left:556;top:1025;width:470;height:308;"><!--$img %ImageAssetImpl:/images/DSC_5666.jpg$--><img src="sitebuilder/images/DSC_5666-470x308.jpg" width="470" height="308" alt=""></div>      <div id="e2" style="position:absolute;left:896;top:2452;width:297;height:453;"><!--$img %ImageAssetImpl:/images/DSC_5671.jpg$--><img src="sitebuilder/images/DSC_5671-297x453.jpg" width="297" height="453" alt=""></div>      <div id="e3" style="position:absolute;left:601;top:1964;width:600;height:399;"><!--$img %ImageAssetImpl:/images/DSC_5673.jpg$--><img src="sitebuilder/images/DSC_5673-600x399.jpg" width="600" height="399" alt=""></div>      <div id="e4" style="position:absolute;left:84;top:1714;width:600;height:399;"><!--$img %ImageAssetImpl:/images/DSC_5675.jpg$--><img src="sitebuilder/images/DSC_5675-600x399.jpg" width="600" height="399" alt=""></div>      <div id="e5" style="position:absolute;left:527;top:1441;width:600;height:399;"><!--$img %ImageAssetImpl:/images/DSC_5676.jpg$--><img src="sitebuilder/images/DSC_5676-600x399.jpg" width="600" height="399" alt=""></div>      <div id="e6" style="position:absolute;left:313;top:2546;width:297;height:453;"><!--$img %ImageAssetImpl:/images/DSC_5682.jpg$--><img src="sitebuilder/images/DSC_5682-297x453.jpg" width="297" height="453" alt=""></div>      <div id="e7" style="position:absolute;left:4;top:2544;width:298;height:455;"><!--$img %ImageAssetImpl:/images/DSC_5690.jpg$--><img src="sitebuilder/images/DSC_5690-298x455.jpg" width="298" height="455" alt=""></div>      <div id="e8" style="position:absolute;left:624;top:2546;width:298;height:453;"><!--$img %ImageAssetImpl:/images/DSC_5693.jpg$--><img src="sitebuilder/images/DSC_5693-298x453.jpg" width="298" height="453" alt=""></div>      <div id="e9" style="position:absolute;left:88;top:2179;width:600;height:399;"><!--$img %ImageAssetImpl:/images/DSC_5701.jpg$--><img src="sitebuilder/images/DSC_5701-600x399.jpg" width="600" height="399" alt=""></div>      <div id="e10" style="position:absolute;left:154;top:1201;width:490;height:322;"><!--$img %ImageAssetImpl:/images/DSC_5703.jpg$--><img src="sitebuilder/images/DSC_5703-490x322.jpg" width="490" height="322" alt=""></div>      <div id="e11" style="position:absolute;left:4;top:1;width:1277;height:599;"><!--$img %ImageAssetImpl:/images/the_crew4.jpg$--><img src="sitebuilder/images/the_crew4-1277x599.jpg" width="1277" height="599" alt=""></div>      <div id="e12" style="position:absolute;left:718;top:1;width:560;height:78;"><span class="text"><span style="font-size:10px;line-height:13px;"><br></span><b><font color="#333333" size="4"><span style="font-size:22px;line-height:26px;">WELCOME</span></font></b><font color="#333333" size="3"><span style="font-size:16px;line-height:19px;"> TO THE DOG AND CAT, the online site of our natural dog and <br soft>cat food and supplies stores in Essex Jct. and Stowe, Vt (Vermont ) We hope <br soft>that you can find us as easily on the web as you can by foot, phone and vehicle.<br soft></span></font></span>      </div>
+      <div id="e13" style="position:absolute;left:1118;top:622;width:145;height:212;"><div style="width:145px;height:212px;overflow:auto;"><!--$begin html$--><!-- Facebook Badge START --><a href="http://www.facebook.com/pages/The-Dog-and-Cat/339137272355" title="The Dog and Cat" target="_TOP" style="font-family: &quot;lucida grande&quot;,tahoma,verdana,arial,sans-serif; font-size: 11px; font-variant: normal; font-style: normal; font-weight: normal; color: #3B5998; text-decoration: none;">The Dog and Cat</a><span style="font-family: &quot;lucida grande&quot;,tahoma,verdana,arial,sans-serif; font-size: 11px; line-height: 16px; font-variant: normal; font-style: normal; font-weight: normal; color: #555555; text-decoration: none;">&nbsp;|&nbsp;</span><a href="http://www.facebook.com/business/dashboard/" title="Make your own badge!" target="_TOP" style="font-family: &quot;lucida grande&quot;,tahoma,verdana,arial,sans-serif; font-size: 11px; font-variant: normal; font-style: normal; font-weight: normal; color: #3B5998; text-decoration: none;">Promote Your Page Too</a><br/><a href="http://www.facebook.com/pages/The-Dog-and-Cat/339137272355" title="The Dog and Cat" target="_TOP"><img src="http://badge.facebook.com/badge/339137272355.4349.1806689650.png" width="137" height="84" style="border: 0px;" /></a><!-- Facebook Badge END --><!--$end html$--></div>
+      </div>
+      <div id="e14" style="position:absolute;left:4;top:1;width:184;height:413;">
+        <table border="0" cellspacing="0" cellpadding="0" width="184">
+          <tr>
+            <td nowrap height="413" align="center" bgcolor="#000000"><span class="text"><font color="#FFCC00" size="7"><span style="font-size:72px;line-height:84px;">THE<br>DOG<br>AND<br>CAT<br soft></span></font></span></td>
+          </tr>
+        </table>
+      </div>
+      <div id="e15" style="position:absolute;left:13;top:400;width:166;height:140;">
+        <table border="0" cellspacing="0" cellpadding="0" width="166">
+          <tr>
+            <td nowrap height="140" align="center" bgcolor="#9999FF"><span class="text"><font color="#000000" size="6"><span style="font-size:30px;line-height:35px;">NATURAL <br soft>PET FOOD <br soft>AND <br soft>SUPPLIES<br soft></span></font></span></td>
+          </tr>
+        </table>
+      </div>
+      <div id="e16" style="position:absolute;left:232;top:616;width:155;height:106;">
+        <table border="0" cellspacing="0" cellpadding="0" width="155">
+          <tr>
+            <td nowrap height="106" align="center" bgcolor="#000000"><span class="text"><b><font color="#FFCC33" size="3"><span style="font-size:16px;line-height:19px;">4 Carmichael St.<br>Suite 109<br>Essex Jct., VT <br soft>05452<br>(802) 872-8900<br soft></span></font></b></span></td>
+          </tr>
+        </table>
+      </div>
+      <div id="e17" style="position:absolute;left:39;top:625;width:150;height:141;"><!--$navbar 
+name=navbar.nav
+assetID=%NavbarAsset:/navigation_bars/navbar.nav
+$--><!--$begin exclude$--><table border="0" cellspacing="0" cellpadding="0"><tr><td><a href="aboutus.html" onMouseOver="document.images['i18'].src='sitebuilder/images/navbar-0-mouseOver-76490.png'" onMouseOut="document.images['i18'].src='sitebuilder/images/navbar-0-inactive-31812.png'"><img name="i18" src="sitebuilder/images/navbar-0-inactive-31812.png" border="0" width="150" height="33" alt=""/></a></td></tr><tr><td height="3" width="0"> <img src="" height="3" width="1" alt=""></td></tr><tr><td><a href="contactus.html" onMouseOver="document.images['i19'].src='sitebuilder/images/navbar-1-mouseOver-76708.png'" onMouseOut="document.images['i19'].src='sitebuilder/images/navbar-1-inactive-32281.png'"><img name="i19" src="sitebuilder/images/navbar-1-inactive-32281.png" border="0" width="150" height="33" alt=""/></a></td></tr><tr><td height="3" width="0"> <img src="" height="3" width="1" alt=""></td></tr><tr><td><a href="products.html" onMouseOver="document.images['i20'].src='sitebuilder/images/navbar-2-mouseOver-76911.png'" onMouseOut="document.images['i20'].src='sitebuilder/images/navbar-2-inactive-32562.png'"><img name="i20" src="sitebuilder/images/navbar-2-inactive-32562.png" border="0" width="150" height="33" alt=""/></a></td></tr><tr><td height="3" width="0"> <img src="" height="3" width="1" alt=""></td></tr><tr><td><a href="index.html" onMouseOver="document.images['i21'].src='sitebuilder/images/navbar-3-mouseOver-77114.png'" onMouseOut="document.images['i21'].src='sitebuilder/images/navbar-3-active-69685.png'"><img name="i21" src="sitebuilder/images/navbar-3-active-69685.png" width="150" height="33" border="0" alt=""/></a></td></tr></table><!--$end exclude$-->      </div>
+      <div id="e22" style="position:absolute;left:686;top:621;width:156;height:96;">
+        <table border="0" cellspacing="0" cellpadding="0" width="156">
+          <tr>
+            <td nowrap height="96" align="center" bgcolor="#000000"><span class="text"><b><font color="#FFCC33" size="3"><span style="font-size:16px;line-height:19px;">1056 Mountain Rd.<br>Stowe, VT 05672<br>(802) 253-9995<br soft></span></font></b></span></td>
+          </tr>
+        </table>
+      </div>
+      <div id="e23" style="position:absolute;left:407;top:631;width:217;height:76;"><!--$addOn style=large
+description=The Dog and Cat in Essex Jct.
+publish.local=false
+street=4 Carmichael St. Suite 108
+element.icon=resource\:/resources/addons/yahooMap/icon-large.gif
+town=Essex Jct Vermont 05452
+addOn=resource\:/resources/addons/yahooMap/yahooMap.addon
+$--><!--$begin exclude$--><img src="http://us.yimg.com/i/geo/ao/m_top2.gif" width=217 height=23 border=0 alt=""><table bgcolor=#666666 cellpadding=1 cellspacing=0 border=0 width=217><tr><td><table bgcolor=#ffffcc cellpadding=6 cellspacing=0 border=0 width=100%><tr><td bgcolor="#ffffcc" align=center><font face="Arial" size=-1><b><a href="http://maps.yahoo.com/py/maps.py?Pyt=Tmap&addr=4 Carmichael St. Suite 108&csz=Essex Jct Vermont 05452&Get+Map=Get+Map">The Dog and Cat in Essex Jct.</a></b><br></font></td></tr></table></td></tr></table><img src="http://us.yimg.com/i/geo/ao/m_bot.gif" width=217 height=23 border=0 alt=""><!--$end exclude$-->      </div>
+      <div id="e24" style="position:absolute;left:871;top:629;width:217;height:76;"><!--$addOn style=large
+description=The Dog and Cat at Stowe
+publish.local=false
+street=1056 Mountain Rd.
+element.icon=resource\:/resources/addons/yahooMap/icon-large.gif
+town=Stowe Vermont 05672
+addOn=resource\:/resources/addons/yahooMap/yahooMap.addon
+$--><!--$begin exclude$--><img src="http://us.yimg.com/i/geo/ao/m_top2.gif" width=217 height=23 border=0 alt=""><table bgcolor=#666666 cellpadding=1 cellspacing=0 border=0 width=217><tr><td><table bgcolor=#ffffcc cellpadding=6 cellspacing=0 border=0 width=100%><tr><td bgcolor="#ffffcc" align=center><font face="Arial" size=-1><b><a href="http://maps.yahoo.com/py/maps.py?Pyt=Tmap&addr=1056 Mountain Rd.&csz=Stowe Vermont 05672&Get+Map=Get+Map">The Dog and Cat at Stowe</a></b><br></font></td></tr></table></td></tr></table><img src="http://us.yimg.com/i/geo/ao/m_bot.gif" width=217 height=23 border=0 alt=""><!--$end exclude$-->      </div>
+<!--$begin exclude$-->
+    </div>
+<!--$end exclude$-->
+  </body>
+</html>
+<!-- text below generated by server. PLEASE REMOVE --><!-- Counter/Statistics data collection code --><script language="JavaScript" src="http://l.yimg.com/d/lib/smb/js/hosting/cp/js_source/whv2_001.js"></script><script language="javascript">geovisit();</script><noscript><img src="http://visit.webhosting.yahoo.com/visit.gif?us1395690088" alt="setstats" border="0" width="1" height="1"></noscript>
