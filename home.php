b44d916136a07012ce8ac0ec12a3d4dd4a0943c9
jennyoc <user@users-MacBook-Pro-3.local>
2014-04-11 11:11:43 -0400

    everything

---
+++ home.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Web Usability Testing</title>
+<meta charset="utf-8">
+<meta name="author" content="Jenny O'Callaghan, Alex Bonfigli">
+<meta name="description" content="Web Usability Testing">
+    
+<link rel="stylesheet"
+      href="dog.css"
+      media="screen">
+    
+</head>
+        <body id="home">
+<article id="main">
+    <h1>Web Usability Test |  <a href="http://www.thedogandcatvt.com/">The Dog and Cat</a></h1>
+    
+  <nav>
+      <ul id='shadowmenu'>
+          <li><a href="http://www.uvm.edu/~jocallag/cs142/assignment6/home.php">Changes</a></li>
+          <li><a href="http://www.uvm.edu/~jocallag/cs142/assignment6/videos.php">Videos</a></li>
+   </ul>
+            </nav>
+ 
+    
+
+    
+
+    <h2>Before...</h2>
+    <div class='before' >    
+      <a href="http://www.uvm.edu/~jocallag/cs142/assignment6/pics/home.jpg" target="_blank"><img src="http://www.uvm.edu/~jocallag/cs142/assignment6/pics/home.jpg" width="300" height="200"  alt="Main Page" /></a>
+      <a href="http://www.uvm.edu/~jocallag/cs142/assignment6/pics/contact.jpg" target="_blank"><img src="http://www.uvm.edu/~jocallag/cs142/assignment6/pics/contact.jpg" width="300" height="200"  alt="Contact Page" /></a>
+      <a href="http://www.uvm.edu/~jocallag/cs142/assignment6/pics/beds.jpg" target="_blank"><img src="http://www.uvm.edu/~jocallag/cs142/assignment6/pics/beds.jpg" width="300" height="200"  alt="Beds Page" /></a>
+      <a href="http://www.uvm.edu/~jocallag/cs142/assignment6/pics/products.jpg" target="_blank"><img src="http://www.uvm.edu/~jocallag/cs142/assignment6/pics/products.jpg" width="300" height="200"  alt="Products Page" /></a>
+    </div>
+            
+    
+             <aside id="other">
+         <div class="container">
+  <h3>5 Changes</h3>
+  <ol class="ordered-list">
+    <li>Removed About Us Page and moved info to Home and Contact Us page</li>
+   <li>Remove Products page and moved info to navbar</li>
+    <li>Created organized sale pages for non-food products</li>
+    <li>Made the site more organized: Restyled headers, restyled navigation, removed tacky gifs, removed pictures from the bottom</li>
+    <li>Created a Gallery of store pictures</li>
+
+
+  </ol>
+</div>
+         </aside>  
+    
+    
+    <h2>After...</h2>
+    <div class='after'>
+          <a href="http://www.uvm.edu/~jocallag/cs142/assignment6/pics/homepage.png" target="_blank"><img src="http://www.uvm.edu/~jocallag/cs142/assignment6/pics/homepage.png" width="300" height="200"  alt="Home Page" /></a>
+      <a href="http://www.uvm.edu/~jocallag/cs142/assignment6/pics/homepage_bottom.png" target="_blank"><img src="http://www.uvm.edu/~jocallag/cs142/assignment6/pics/homepage_bottom.png" width="300" height="200"  alt="Home Page" /></a>
+      <a href="http://www.uvm.edu/~jocallag/cs142/assignment6/pics/productmenu.png" target="_blank"><img src="http://www.uvm.edu/~jocallag/cs142/assignment6/pics/productmenu.png" width="300" height="200"  alt="New Nav" /></a>
+      <a href="http://www.uvm.edu/~jocallag/cs142/assignment6/pics/product.png" target="_blank"><img src="http://www.uvm.edu/~jocallag/cs142/assignment6/pics/product.png" width="300" height="200"  alt="Products Page" /></a>
+      <a href="http://www.uvm.edu/~jocallag/cs142/assignment6/pics/gallery.png" target="_blank"><img src="http://www.uvm.edu/~jocallag/cs142/assignment6/pics/gallery.png" width="300" height="200"  alt="Gallery Page" /></a>
+
+    </div>
+            
+    
+        </article>
+        
+         <div class="widget arrow-up"> <p>The disorganization of the product page was brought up a lot in the usability tests so we decided that removing that page entirely and moving the list of products to the navbar would make it look a lot nicer. Also on the original products page they have two unclearly labeled lists. One is a list of food brands and the other is a list of non-food products. We decided to put all the food brands under the same button called “foods” in the navbar.</p> </div>
+ 
+
+         
+        
+         
+         
+<footer>
+    <p>Jenny O'Callaghan and Alex Bonfigli</p>
+</footer>
+    
+
+</body>
+</html>
---
+++ videos.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Web Usability Testing</title>
+<meta charset="utf-8">
+<meta name="author" content="Jenny O'Callaghan, Alex Bonfigli">
+<meta name="description" content="Web Usability Testing">
+    
+<link rel="stylesheet"
+      href="dog.css"
+      media="screen">
+    
+</head>
+<body id="videos">
+<article id="main">
+    <h1>Web Usability Test |  <a href="http://www.thedogandcatvt.com/">The Dog and Cat</a></h1>
+    
+  <nav>
+      <ul id='shadowmenu'>
+          <li><a href="http://www.uvm.edu/~jocallag/cs142/assignment6/home.php">Changes</a></li>
+          <li><a href="http://www.uvm.edu/~jocallag/cs142/assignment6/videos.php">Videos</a></li>
+ </ul>
+        </nav>
+
+    <h2>Videos</h2>
+        <div class="script">
+        <a href="http://www.uvm.edu/~jocallag/cs142/assignment6/Script.pdf">Script</a></div>
+        
+        <div class="form">
+        <a href="http://www.uvm.edu/~jocallag/cs142/assignment6/form.pdf">Permission Form</a></div>
+        
+        <div class="videos"><ol>
+                <li> <iframe width="560" height="315" src="//www.youtube.com/embed/Ed8kpl6hwYc" frameborder="0" allowfullscreen></iframe></li>
+            
+                <li><iframe width="560" height="315" src="//www.youtube.com/embed/Sctd3y70-60" frameborder="0" allowfullscreen></iframe></li>
+            
+                <li><iframe width="560" height="315" src="//www.youtube.com/embed/ot6QG6vOEHY" frameborder="0" allowfullscreen></iframe></li>
+            </ol>
+
+</div>
+
+        
+        
+<footer>
+    <p>Jenny O'Callaghan and Alex Bonfigli</p>
+</footer>
+
+
+    </body>
